6b2b316f2d27ff8fc7bce76913f66684e2acc7d7
Jack Holborn <holborn79@gmail.com>
2012-05-07 02:58:28 +1000

    Update fsniper & ranger

--- .config/ranger/options.py
+++ .config/ranger/options.py
@@ -1,21 +1,18 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2009, 2010, 2011  Roman Zimbelmann <romanz@lavabit.com>
-# This configuration file is licensed under the same terms as ranger.
-# ===================================================================
-# This is the main configuration file of ranger.  It consists of python
-# code, but fear not, you don't need any python knowledge for changing
-# the settings.
-#
-# Lines beginning with # are comments.  To enable a line, remove the #.
-#
-# You can customize ranger in the file ~/.config/ranger/options.py.
-# It has the same syntax as this file.  In fact, you can just copy this
-# file there with `ranger --copy-config=options' and make your modifications.
-# But make sure you update your configs when you update ranger.
-# ===================================================================
-
 from ranger.api.options import *
+
+# Use that external preview script or display internal plain text previews?
+use_preview_script = True
+
+# Preview files on the rightmost column?
+# And collapse (shrink) the last column if there is nothing to preview?
+collapse_preview = False
+
+# Display the directory name in tabs?
+dirname_in_tabs = True
 
 # Enable the mouse support?
 mouse_enabled = False
 
+# Display the file size in the main column or status bar?
+display_size_in_main_column = True
+display_size_in_status_bar = True
